6772a2ba6887221fa3039992e0477d8cc13e6723
codingjin <475166289@qq.com>
2024-11-17 21:09:57 -0700

    GFLOPs/s

--- mm_1.c
+++ mm_1.c
@@ -4,6 +4,8 @@
 #include "time.h"
 #include "cblas.h"
 
+#define ROUND 5
+
 int main(int argc, char* argv[])
 {
 	if (argc < 4) {
@@ -29,22 +31,25 @@ int main(int argc, char* argv[])
 	double *C = (double*)malloc(sizeof(double) * sizeofc);
 
 	srand(113);
+	double alpha = ((double)rand()) / RAND_MAX;
+	double beta = ((double)rand()) / RAND_MAX;
 	for (int i = 0; i < sizeofa; i++)	A[i] = ((double)rand()) / RAND_MAX;
 	for (int i = 0; i < sizeofb; i++)	B[i] = ((double)rand()) / RAND_MAX;
 	for (int i = 0; i < sizeofc; i++)	C[i] = ((double)rand()) / RAND_MAX;
 	
 	printf("M=%d N=%d K=%d\n", m, n, k);
-	for (int round = 0; round < 5; round++) {
+	for (int round = 0; round < ROUND; round++) {
 		gettimeofday(&start, NULL);
-		cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, m, n, k, 1, A, m, B, k, 0, C, m);
+		cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, m, n, k, alpha, A, m, B, k, beta, C, m);
 		gettimeofday(&finish, NULL);
 
 		duration = ((long)(finish.tv_sec - start.tv_sec))*1000000 + (long)(finish.tv_usec - start.tv_usec);
 		printf("Round %d: %ld ms\n", round, duration/1000);
 		accumulate_duration += duration;
 	}
-	long average_duration = accumulate_duration / 5000;
-	printf("Average: %ld ms\n", average_duration);
+	long average_duration = accumulate_duration / ROUND; // 1 us = 1.0e-6 s
+	printf("M * N * (1+3*K) = %ld flops, time = %ld us\n", (long)m*(long)n*(long)(1+3*k), average_duration);
+	printf("Performance: %.3f GFLOPs/s\n", 1.0e-3*m*n*(1+3*k) / average_duration);
 	
 	free(A);
 	free(B);
