b7a0e7482c84ba38e83a8a7cef4e184d2269b95a
codingjin <475166289@qq.com>
2024-11-18 14:03:18 -0700

    2mnk

--- mm.c
+++ mm.c
@@ -8,16 +8,17 @@
 
 int main(int argc, char* argv[])
 {
-	if (argc < 4) {
-		printf("Input error, ./mm M N K\n");
+	if (argc != 5) {
+		printf("Input error, ./mm <THREAD_NUM> M N K\n");
 		return 1;
 	}
+	
+	int thread_num = atoi(argv[1]);
+	int m = atoi(argv[2]);
+	int n = atoi(argv[3]);
+	int k = atoi(argv[4]);
 
-	openblas_set_num_threads(1);
-
-	int m = atoi(argv[1]);
-	int n = atoi(argv[2]);
-	int k = atoi(argv[3]);
+	openblas_set_num_threads(thread_num);
 	int sizeofa = m * k;
 	int sizeofb = k * n;
 	int sizeofc = m * n;
@@ -37,23 +38,22 @@ int main(int argc, char* argv[])
 	for (int i = 0; i < sizeofb; i++)	B[i] = ((double)rand()) / RAND_MAX;
 	for (int i = 0; i < sizeofc; i++)	C[i] = ((double)rand()) / RAND_MAX;
 	
-	printf("M=%d N=%d K=%d\n", m, n, k);
+	//printf("ThreadNum=%d M=%d N=%d K=%d\n", thread_num, m, n, k);
 	for (int round = 0; round < ROUND; round++) {
 		gettimeofday(&start, NULL);
 		cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, m, n, k, alpha, A, m, B, k, beta, C, m);
 		gettimeofday(&finish, NULL);
 
 		duration = ((long)(finish.tv_sec - start.tv_sec))*1000000 + (long)(finish.tv_usec - start.tv_usec);
-		printf("Round %d: %ld ms\n", round, duration/1000);
+		//printf("Round %d: %ld ms\n", round, duration/1000);
 		accumulate_duration += duration;
 	}
 	long average_duration = accumulate_duration / ROUND; // 1 us = 1.0e-6 s
-	printf("M * N * (1+3*K) = %ld flops, time = %ld us\n", (long)m*(long)n*(long)(1+3*k), average_duration);
-	printf("Performance: %.3f GFLOPs/s\n", 1.0e-3*m*n*(1+3*k) / average_duration);
-	
+	printf("ThreadNum=%d M=%d N=%d K=%d, Performance: %.3f GFLOPs/s\n\n", thread_num, m, n, k, 2.0e-3*m*n*k / average_duration);
 	free(A);
 	free(B);
 	free(C);
 	return 0;
+
 }
 
